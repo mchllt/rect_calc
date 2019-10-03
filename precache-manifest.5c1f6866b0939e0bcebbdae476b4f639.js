d7ee81286bd485aad9db59b29264622b8f6622c8
mchllt <traquenamichelle01@yahoo.com>
2019-10-03 21:27:11 +0800

    Updates

---
+++ precache-manifest.5c1f6866b0939e0bcebbdae476b4f639.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3078ed18b46f2950a8e2f968717d75cb",
+    "url": "/rect_calc/index.html"
+  },
+  {
+    "revision": "b4e66f12b9d30c978731",
+    "url": "/rect_calc/static/css/main.8a5900f7.chunk.css"
+  },
+  {
+    "revision": "ba6ba07847f8a75ce5d3",
+    "url": "/rect_calc/static/js/2.7d71e644.chunk.js"
+  },
+  {
+    "revision": "b4e66f12b9d30c978731",
+    "url": "/rect_calc/static/js/main.f782ff31.chunk.js"
+  },
+  {
+    "revision": "05cefc3e417c94653ef2",
+    "url": "/rect_calc/static/js/runtime-main.a2c39c7a.js"
+  }
+]);
